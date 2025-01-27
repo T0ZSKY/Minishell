0f1b5017bfe90ac90823bbd43893fc849d845852
Tom Limon <tomlimon1508@gmail.com>
2025-01-27 23:45:36 +0100

    rework no builtin

--- srcs/executor/executor.c
+++ srcs/executor/executor.c
@@ -6,63 +6,75 @@
 /*   By: tomlimon <tom.limon@>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 18:10:24 by tomlimon          #+#    #+#             */
-/*   Updated: 2025/01/27 18:16:32 by tomlimon         ###   ########.fr       */
+/*   Updated: 2025/01/27 23:45:13 by tomlimon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/header/minishell.h"
 
-void ft_cmd(char **tab, char **envp)
+char **copy_arguments(char **tab)
 {
-	pid_t pid;
-	int status;
-	char *cmd_path;
-	char **args_copy = NULL;
+	char **args_copy;
 	int i = 0;
 
 	while (tab[i])
 		i++;
 	args_copy = malloc(sizeof(char *) * (i + 1));
 	if (!args_copy)
-		return;
+		return (NULL);
 
 	i = 0;
-	while (tab[i]) {
+	while (tab[i])
+	{
 		args_copy[i] = ft_strdup(tab[i]);
 		if (!args_copy[i]) {
 			ft_free_tab(args_copy);
-			return;
+			return (NULL);
 		}
 		i++;
 	}
 	args_copy[i] = NULL;
+	return (args_copy);
+}
 
+void execute_child(char **args_copy, char **envp)
+{
+	char *cmd_path;
+
+	cmd_path = find_command_path(args_copy[0]);
+	if (!cmd_path)
+	{
+		printf("%s: command not found\n", args_copy[0]);
+		ft_free_tab(args_copy);
+		exit(127);
+	}
+	if (execve(cmd_path, args_copy, envp) == -1) {
+		perror("execve");
+		free(cmd_path);
+		ft_free_tab(args_copy);
+		exit(126);
+	}
+	free(cmd_path);
+}
+
+void ft_cmd(char **tab, char **envp)
+{
+	pid_t pid;
+	char **args_copy;
+
+	args_copy = copy_arguments(tab);
+	if (!args_copy)
+		return;
 	pid = fork();
 	if (pid == -1) {
 		perror("fork");
 		ft_free_tab(args_copy);
 		return;
 	}
-
 	if (pid == 0) {
-		cmd_path = find_command_path(args_copy[0]);
-		if (!cmd_path) {
-			write(2, args_copy[0], strlen(args_copy[0]));
-			write(2, ": command not found\n", 20);
-			ft_free_tab(args_copy);
-			exit(127);
-		}
-
-		if (execve(cmd_path, args_copy, envp) == -1) {
-			perror("execve");
-			free(cmd_path);
-			ft_free_tab(args_copy);
-			exit(126);
-		}
+		execute_child(args_copy, envp);
 	} else {
 		ft_free_tab(args_copy);
-		waitpid(pid, &status, 0);
-		if (WIFEXITED(status))
-			status = WEXITSTATUS(status);
+		waitpid(pid, NULL, 0);
 	}
 }
