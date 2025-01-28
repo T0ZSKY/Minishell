cdaf4dd82fadc1a302a5121fcb11639ce4fcbc16
Taki-do <malbecktao@gmail.com>
2025-01-28 10:01:18 +0100

    nothing

--- srcs/parser/lexer.c
+++ srcs/parser/lexer.c
@@ -3,26 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   lexer.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tomlimon <tom.limon@>                      +#+  +:+       +#+        */
+/*   By: taomalbe <taomalbe@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 17:48:48 by tomlimon          #+#    #+#             */
-/*   Updated: 2025/01/27 23:50:49 by tomlimon         ###   ########.fr       */
+/*   Updated: 2025/01/27 18:42:36 by taomalbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/header/minishell.h"
 
-void ft_lexer(char *input, t_shell *shell, char **envp) // fonction qui peux géré les entré
+void	ft_lexer(char *input, t_shell *shell) // fonction qui peux géré les entré
 {
+	if (!input)
+		return ;
 	shell->tab = ft_split(input, ' ');
-
 	if (ft_strcmp(shell->tab[0], "echo") == 0)
 		ft_echo(shell->tab);
-	else if (ft_strcmp(shell->tab[0], "cd") == 0)
-		ft_cd(shell->tab);
-	//sinon appelé fonction qui gere fonction non builtin
-	else
-	{
-		ft_cmd(shell->tab, envp);
-	}
 }
