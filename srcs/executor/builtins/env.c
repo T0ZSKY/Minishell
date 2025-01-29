2dd74ec96ec3b5c5fd59b83b365bc05cba597405
Tom Limon <tomlimon1508@gmail.com>
2025-01-29 14:55:51 +0100

    add check verif env

--- srcs/executor/builtins/env.c
+++ srcs/executor/builtins/env.c
@@ -6,20 +6,22 @@
 /*   By: tomlimon <tom.limon@>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 14:17:21 by tomlimon          #+#    #+#             */
-/*   Updated: 2025/01/29 14:36:14 by tomlimon         ###   ########.fr       */
+/*   Updated: 2025/01/29 14:53:53 by tomlimon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/header/minishell.h"
 
-void ft_env(char **tab, t_shell *shell)
+void	ft_env(char **tab, t_shell *shell)
 {
-	int i;
+	int	i;
 
+	(void)tab;
 	i = 0;
-	while (shell->envp[i])
+	while (shell->envp && shell->envp[i])
 	{
-		printf("%s\n", shell->envp[i]);
+		if (strchr(shell->envp[i], '='))
+			printf("%s\n", shell->envp[i]);
 		i++;
 	}
 }
