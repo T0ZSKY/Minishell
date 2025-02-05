1361afc65d8cb5d9d330c00bbb772b71b433656b
Takido-NFT <tmalbeck@DESKTOP-V1QIJ5K>
2025-02-05 14:50:28 +0100

    before merge

--- srcs/parser/lexer.c
+++ srcs/parser/lexer.c
@@ -6,7 +6,7 @@
 /*   By: taomalbe <taomalbe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 17:48:48 by tomlimon          #+#    #+#             */
-/*   Updated: 2025/02/05 11:41:10 by taomalbe         ###   ########.fr       */
+/*   Updated: 2025/02/05 11:47:50 by taomalbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,7 +99,6 @@ void	ft_lexer(char *input, t_shell *shell)
 	if (is_complex(input))
 	{
 		split = ft_split(new_input, '|');
-		//split = skip_pipes(split);
 		free(new_input);
 		
 		//redirection pas prise en compte faire une fonction qui les prends en compte avant d'exec pipes
