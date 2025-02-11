185776e9e88f176ba57e48dbf5433b59fc4153f0
Tom Limon <tomlimon1508@gmail.com>
2025-02-11 17:31:32 +0100

    norn

--- srcs/executor/builtins/unset.c
+++ srcs/executor/builtins/unset.c
@@ -6,37 +6,44 @@
 /*   By: tomlimon <tomlimon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 16:42:54 by tomlimon          #+#    #+#             */
-/*   Updated: 2025/02/11 15:53:54 by tomlimon         ###   ########.fr       */
+/*   Updated: 2025/02/11 17:31:08 by tomlimon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/header/minishell.h"
 
-void	ft_unset(char **tab, t_shell *shell)
+static void	remove_env_var(char *var, t_shell *shell)
 {
-	int	i;
 	int	j;
 	int	len;
 
-	i = 1;
-	while (tab[i++])
+	j = 0;
+	len = ft_strlen(var);
+	while (shell->envp[j])
 	{
-		j = 0;
-		len = ft_strlen(tab[i]);
-		while (shell->envp[j])
+		if (ft_strncmp(shell->envp[j], var, len) == 0)
 		{
-			if (ft_strncmp(shell->envp[j], tab[i], len) == 0)
+			free(shell->envp[j]);
+			while (shell->envp[j + 1])
 			{
-				free(shell->envp[j]);
-				while (shell->envp[j + 1])
-				{
-					shell->envp[j] = shell->envp[j + 1];
-					j++;
-				}
-				shell->envp[j] = NULL;
-				break ;
+				shell->envp[j] = shell->envp[j + 1];
+				j++;
 			}
-			j++;
+			shell->envp[j] = NULL;
+			break ;
 		}
+		j++;
+	}
+}
+
+void	ft_unset(char **tab, t_shell *shell)
+{
+	int	i;
+
+	i = 1;
+	while (tab[i])
+	{
+		remove_env_var(tab[i], shell);
+		i++;
 	}
 }
