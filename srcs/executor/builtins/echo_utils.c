d526dfdf720dc9825f0388e98eef04fe6085e1a9
Tom Limon <tomlimon1508@gmail.com>
2025-02-19 22:37:56 +0100

    update

--- srcs/executor/builtins/echo_utils.c
+++ srcs/executor/builtins/echo_utils.c
@@ -6,7 +6,7 @@
 /*   By: tomlimon <tomlimon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 23:47:35 by tomlimon          #+#    #+#             */
-/*   Updated: 2025/02/11 15:11:41 by tomlimon         ###   ########.fr       */
+/*   Updated: 2025/02/19 22:37:16 by tomlimon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,22 @@ void	print_clean_str(char *str, int has_next)
 
 int	handle_n_flag(char **tab, int *j)
 {
-	if (tab[1] && ft_strcmp(tab[1], "-n") == 0)
+	int	found_n_flag;
+	int	i;
+
+	found_n_flag = 0;
+	while (tab[*j] && tab[*j][0] == '-')
 	{
-		*j = 2;
-		return (1);
+		i = 1;
+		while (tab[*j][i] == 'n')
+			i++;
+		if (tab[*j][i] == '\0')
+		{
+			found_n_flag = 1;
+			(*j)++;
+		}
+		else
+			break ;
 	}
-	return (0);
+	return (found_n_flag);
 }
